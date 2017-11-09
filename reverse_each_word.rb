806fd312c7a162555f311e91da968eec7e0b138e
Steven Senkowski <scsenkowski@gmail.com>
2017-11-09 17:53:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,5 @@ def reverse_each_word(phrase)
   phrase = "Hello there, and how are you?"
   phrase_2_array = phrase.split("")
   phrase_array_reversed = phrase_2_array.reverse
-  phrase_final = phrase_array_reversed.join
-  phrase_final.reverse
+  phrase_array_reversed.join
 end
