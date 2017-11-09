dbd03034e5e95677d8fe51250091ee6e183434d2
Steven Senkowski <scsenkowski@gmail.com>
2017-11-09 18:39:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ def  reverse_each_word(phrase)
   my_phrase = []
   phrase1 = phrase
   phrase_2_array = phrase1.split(" ")
-  phrase_2_array.map do |word|
+  phrase_2_array.collect do |word|
     my_phrase.push("#{word}".reverse)
   end
   my_phrase.join(" ")
