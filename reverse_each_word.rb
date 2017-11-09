fbb7e37a6cf32f3d9d55286711d7aa3dbb8bc745
Steven Senkowski <scsenkowski@gmail.com>
2017-11-09 18:17:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   backwards_words = []
   phrase = "Hello there, and how are you?"
-  phrase_2_array.each do |word|
+  phrase.each do |word|
     backwards_words = "#{word}".reverse
   end
   backwards_words
